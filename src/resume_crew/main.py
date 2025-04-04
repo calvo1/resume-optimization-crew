7e0bc5c0816e418c44659e82378d8420c5e158f9
calvo1 <muthwangaine@gmail.com>
2025-04-04 13:36:39 -0500

    init2

--- src/resume_crew/main.py
+++ src/resume_crew/main.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python
 import sys
 import warnings
-
 from resume_crew.crew import ResumeCrew
 
 warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+
 
 def run():
     """
     Run the resume optimization crew.
     """
     inputs = {
-        'job_url': 'https://www.mckinsey.com/careers/search-jobs/jobs/associate-15178',
-        'company_name': 'Mckinsey & Co.'
+        "job_url": "https://www.linkedin.com/jobs/view/4132705547/?refId=8fd9bad3-622f-4a66-85ae-ecf72a8c99e2&trackingId=lJm44rZ7TCycAjJ%2FEbhu7w%3D%3D",
+        "company_name": "Harnham",
     }
     ResumeCrew().crew().kickoff(inputs=inputs)
+
 
 if __name__ == "__main__":
     run()
